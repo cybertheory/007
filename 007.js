98dee800a27221d7c469fb0364aeb49ba5346066
Rishabh Singh <rishabhspro@gmail.com>
2018-04-22 12:08:10 -0400

    Update 007.js

--- 007.js
+++ 007.js
@@ -1,4 +1,5 @@
 var elements = document.getElementsByClassName("007");
+console.log(elements);
 var results = blockspring.runParsed("query-public-google-spreadsheet", { "query": "Select B", "url": "https://docs.google.com/spreadsheets/d/1m0-4Db1n_eCaI4z5CUZDf5e1VMCnxazBIlhPDWH0i8E/edit?usp=sharing" }, { "api_key": "br_87671_5168dae487bab0e75c74679ee72d0221b8d60c82" }, function(res){
     console.log(res.params);
   })
