76f67726360a52b9e7b9d080839eb5e5fc3b6238
Rishabh Singh <rishabhspro@gmail.com>
2018-04-22 12:22:15 -0400

    Update 007.js

--- 007.js
+++ 007.js
@@ -1,13 +1,17 @@
-var elements = document.getElementsByClassName("007");
-document.write(elements);
-blockspring.runParsed("query-public-google-spreadsheet", { "query": "Select B", "url": "https://docs.google.com/spreadsheets/d/1m0-4Db1n_eCaI4z5CUZDf5e1VMCnxazBIlhPDWH0i8E/edit?usp=sharing" }, { "api_key": "br_87671_5168dae487bab0e75c74679ee72d0221b8d60c82" }, function(res){
-    results=res.params;
-    document.write(res.params;)
-  });
-var i;
-var j = Math.floor(Math.random()) * (results.length);
-doument.write(j);
-for(i = 0; i<elements.length; i++)
+function f1() 
 {
-  elements[i].setAttribute("href", results[j]);
+    var elements = document.getElementsByClassName("007");
+    document.write(elements);
+    var results;
+    blockspring.runParsed("query-public-google-spreadsheet", { "query": "Select B", "url": "https://docs.google.com/spreadsheets/d/1m0-4Db1n_eCaI4z5CUZDf5e1VMCnxazBIlhPDWH0i8E/edit?usp=sharing" }, { "api_key": "br_87671_5168dae487bab0e75c74679ee72d0221b8d60c82" }, function(res){
+        results=res.params;
+        document.write(res.params;)
+      });
+    var i;
+    var j = Math.floor(Math.random()) * (results.length);
+    doument.write(j);
+    for(i = 0; i<elements.length; i++)
+    {
+      elements[i].setAttribute("href", results[j]);
+    }
 }
